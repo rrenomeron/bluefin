19173fbd7f4ca7234a461cbdefe405d63cf2d112
Rich Renomeron <rrenomeron+github@gmail.com>
2025-06-05 22:08:39 -0400

    fix: new icon number for framework in  logo menu

--- system_files/shared/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
+++ system_files/shared/usr/share/ublue-os/user-setup.hooks.d/10-theming.sh
@@ -11,7 +11,7 @@ VEN_ID="$(cat /sys/devices/virtual/dmi/id/chassis_vendor)"
 if [[ ":Framework:" =~ :$VEN_ID: ]]; then
 	echo 'Setting Framework logo menu'
 	dconf write /org/gnome/shell/extensions/Logo-menu/symbolic-icon true
-	dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 31
+	dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 32
 	echo 'Setting touch scroll type'
 	dconf write /org/gnome/desktop/peripherals/mouse/natural-scroll true
 	if [[ $SYS_ID == "Laptop ("* ]]; then
@@ -25,5 +25,5 @@ SYS_ID="$(cat /sys/devices/virtual/dmi/id/product_name)"
 if [[ ":Thelio Astra:" =~ :$SYS_ID: ]]; then
 	echo 'Setting Ampere Logo'
  	dconf write /org/gnome/shell/extensions/Logo-menu/symbolic-icon true
-	dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 32
+	dconf write /org/gnome/shell/extensions/Logo-menu/menu-button-icon-image 33
  fi
